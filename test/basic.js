4e6a6c8df87c7835779eaa2eaf2b9060208588b9
HDegroote <75906619+HDegroote@users.noreply.github.com>
2023-07-24 13:54:32 +0200

    Add tests for update/updating events and callbacks

--- test/basic.js
+++ test/basic.js
@@ -1,7 +1,8 @@
 const test = require('brittle')
+const tmp = require('test-tmp')
+const { once } = require('events')
 const Updater = require('../')
 const { createDrives, eventFlush } = require('./helpers')
-const tmp = require('test-tmp')
 
 test('basic full swap updates', async function (t) {
   const directory = await tmp(t)
@@ -20,31 +21,25 @@ test('basic full swap updates', async function (t) {
   t.is(u.checkout.length, 0, 'empty drive')
   t.is(u.swapNumber, 0, 'using swap 0')
 
-  await updateBin()
+  await updateBin(drive, u, tick++)
 
   t.is(u.checkout.length, drive.core.length, 'up to date')
   t.is(u.swapNumber, 1, 'using swap 1')
 
-  await updateBin()
+  await updateBin(drive, u, tick++)
 
   t.is(u.checkout.length, drive.core.length, 'up to date')
   t.is(u.swapNumber, 2, 'using swap 2')
 
-  await updateBin()
+  await updateBin(drive, u, tick++)
 
   t.is(u.checkout.length, drive.core.length, 'up to date')
-  t.is(u.swapNumber, 3, 'using swpa 3')
+  t.is(u.swapNumber, 3, 'using swap 3')
 
-  await updateBin()
+  await updateBin(drive, u, tick++)
 
   t.is(u.checkout.length, drive.core.length, 'up to date')
   t.is(u.swapNumber, 1, 'skipping swap 0 as its in use and using swap 1')
-
-  async function updateBin () {
-    await drive.put('/by-arch/universal-universal/bin/file', '' + (tick++))
-    await eventFlush()
-    await u.update()
-  }
 })
 
 test('basic non-swap updates', async function (t) {
@@ -64,21 +59,15 @@ test('basic non-swap updates', async function (t) {
   t.is(u.checkout.length, 0, 'empty drive')
   t.is(u.swapNumber, 0, 'using swap 0')
 
-  await updateNonBin()
+  await updateNonBin(drive, u, tick++)
 
   t.is(u.checkout.length, drive.core.length, 'up to date')
   t.is(u.swapNumber, 0, 'still using swap 0')
 
-  await updateNonBin()
+  await updateNonBin(drive, u, tick++)
 
   t.is(u.checkout.length, drive.core.length, 'up to date')
   t.is(u.swapNumber, 0, 'still using swap 0')
-
-  async function updateNonBin () {
-    await drive.put('/some-file', '' + (tick++))
-    await eventFlush()
-    await u.update()
-  }
 })
 
 test('some non-swap, then swap, then non-swap updates', async function (t) {
@@ -98,30 +87,87 @@ test('some non-swap, then swap, then non-swap updates', async function (t) {
   t.is(u.checkout.length, 0, 'empty drive')
   t.is(u.swapNumber, 0, 'using swap 0')
 
-  await updateNonBin()
+  await updateNonBin(drive, u, tick++)
 
   t.is(u.checkout.length, drive.core.length, 'up to date')
   t.is(u.swapNumber, 0, 'still using swap 0')
 
-  await updateBin()
+  await updateBin(drive, u, tick++)
 
   t.is(u.checkout.length, drive.core.length, 'up to date')
   t.is(u.swapNumber, 1, 'using swap 1')
 
-  await updateNonBin()
+  await updateNonBin(drive, u, tick++)
 
   t.is(u.checkout.length, drive.core.length, 'up to date')
   t.is(u.swapNumber, 1, 'still using swap 1')
-
-  async function updateNonBin () {
-    await drive.put('/some-file', '' + (tick++))
-    await eventFlush()
-    await u.update()
-  }
-
-  async function updateBin () {
-    await drive.put('/by-arch/universal-universal/bin/file', '' + (tick++))
-    await eventFlush()
-    await u.update()
-  }
 })
+
+test('updating and update events are triggered', async function (t) {
+  const directory = await tmp(t)
+  const [drive, clone] = await createDrives(t)
+
+  const u = new Updater(clone, {
+    directory,
+    platform: 'universal',
+    arch: 'universal'
+  })
+
+  let updatingCalled = false
+  u.on('updating', () => {
+    if (!u.updating) t.fail('Should be updating when updating-event emitted')
+    updatingCalled = true
+  })
+
+  // Can be triggered multiple times. We just test
+  // that it triggers at least once
+  const updateProm = once(u, 'update')
+
+  await u.ready()
+  await updateBin(drive, u, 0)
+
+  await updateProm
+  t.ok(updatingCalled, 'update called after updating')
+})
+
+test('updating and update callbacks are called', async function (t) {
+  t.plan(1)
+
+  const directory = await tmp(t)
+  const [drive, clone] = await createDrives(t)
+
+  let updatingCalled = false
+  const u = new Updater(clone, {
+    directory,
+    platform: 'universal',
+    arch: 'universal',
+    onupdating: () => {
+      // TODO: decide whether updating should be true while
+      // running this callback (currently false, as this is
+      // the first async call)
+      updatingCalled = true
+    },
+    onupdate: async () => {
+      // Can be called multiple times, we just test
+      // that it is called at least once
+      if (!u.closing) t.ok(updatingCalled, 'onupdate called, after onupdating')
+      await u.close()
+    }
+  })
+
+  await u.ready()
+
+  await updateBin(drive, u, 0)
+})
+
+async function updateBin (drive, u, tick) {
+  await drive.put('/by-arch/universal-universal/bin/file', '' + (tick))
+  await eventFlush()
+  await u.update()
+}
+
+async function updateNonBin (drive, u, tick) {
+  await drive.put('/some-file', '' + (tick))
+  await eventFlush()
+  await u.update()
+}
