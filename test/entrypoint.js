2c05fa85b401c2a80c408cbd3fc7e62753da8c64
HDegroote <75906619+HDegroote@users.noreply.github.com>
2023-07-26 15:50:26 +0200

    Add test for package.json main file being put on disk (#8)
    * Add test for self-defined main
    
    * Simplify (no need for empty exports)
    
    * Extract compile helper

--- test/entrypoint.js
+++ test/entrypoint.js
@@ -23,9 +23,37 @@ test('updates entrypoint on disk and writes /checkout.js', async function (t) {
   await touchAndUpdate('/index.js', 'module.exports = require("./checkout.js")')
 
   const entrypoint = await fsp.readFile(path.join(u.swap, 'index.js'), 'utf-8')
-  const checkout = new Function('require', 'return ' + entrypoint)(require) // eslint-disable-line
+  const checkout = compile(entrypoint)
 
   t.is(checkout.length, drive.core.length)
   t.is(checkout.fork, drive.core.fork)
   t.is(checkout.key, drive.core.id)
 })
+
+test('file referenced in package.json main is put on disk', async function (t) {
+  const directory = await tmp(t)
+  const [drive, clone] = await createDrives(t)
+
+  const u = new Updater(clone, {
+    directory,
+    platform: 'universal',
+    arch: 'universal'
+  })
+
+  const touchAndUpdate = createTouch(drive, u)
+
+  await touchAndUpdate('/checkout.js', '')
+  await touchAndUpdate('/own-main.js', 'module.exports = require("./checkout.js")')
+  await touchAndUpdate('/package.json', JSON.stringify({ main: 'own-main.js' }))
+
+  const entrypoint = await fsp.readFile(path.join(u.swap, 'own-main.js'), 'utf-8')
+  const checkout = compile(entrypoint)
+
+  t.is(checkout.length, drive.core.length)
+  t.is(checkout.fork, drive.core.fork)
+  t.is(checkout.key, drive.core.id)
+})
+
+function compile (entrypoint) {
+  return new Function('require', 'return ' + entrypoint)(require) // eslint-disable-line
+}
